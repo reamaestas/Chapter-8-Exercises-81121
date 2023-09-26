caaffdcf230f15b39e296df24e5b555409ab2f96
reamaestas <reamaestas@yahoo.com>
2023-09-26 21:32:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# ---- String Methods and Data Types ----
+
+num = 1001
+
+# Throws an error:
+print(len(num))
+
+# 1. Use type conversion to print the length (number of digits) of an integer.
+
+
+
+
+# 2. Print the length of 'num'
+
+
+
+
+# 3. Print the number of digits in a FLOAT value (e.g. num = 123.45 has 5 digits but a length of 6).
+
+
+
+
+# 4. Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
